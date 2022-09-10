c7ba106d04fc5d42f80b1342bb888baa046a9c34
peluola <marriegold362@gmail.com>
2022-09-10 16:51:24 -0700

    Task 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - Inputs text
+ *
+ * RETURN: Always 1 (Successful)
+ */
+int main(void)
+{
+	write(2,"and that piece of art useful\" - DORA Korpar,2015-10-19\n",59);
+	return (1);
+}
