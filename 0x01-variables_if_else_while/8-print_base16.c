d4d5ad49c0cea1fb97e85935c93f84015d846f78
peluola <marriegold362@gmail.com>
2022-09-12 19:21:26 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,17 +5,17 @@
  *
  * Return: Always (Success)
  */
-int main (void)
+int main(void)
 {
 
 	char c;
 
-	for (c = '0'; c <= '9'; c++)
+	for(c = '0'; c <= '9'; c++)
 	{
 		putchar(c);
 	}
 
-	for (c = 'a'; c <= 'f'; c++)
+	for(c = 'a'; c <= 'f'; c++)
 	{
 	putchar(c);
         }
