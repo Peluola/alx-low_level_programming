1ac762cf18f39f82a3b061d06bcdaaad42ae6c23
peluola <marriegold362@gmail.com>
2022-09-14 09:32:21 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,10 +3,8 @@
 /**
  *
  * jack_bauer - prints every minute of the day
- * 
  *
  * 
- *
  *
  * Return: every minute of the day of Jack Bauer
  *
@@ -17,16 +15,12 @@ void jack_bauer(void)
 
 	for (H = 0; H <= 2; H++)
 	{
-		
 		for (h = 0; h <= 9; h++)
 		{
-
 			for (M = 0; M < 6; M++)
 			{
-
 				for (m = 0; m <= 9; m++)
 				{
-
 					if (!((H == 2) && (h > 3)))
 					{
 						_putchar('0' + H);
