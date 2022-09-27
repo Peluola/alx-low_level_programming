792335c518e4bdcbf0e48153a9707632546d2619
peluola <marriegold362@gmail.com>
2022-09-26 22:56:02 -0700

    Task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strspn - gets lengt of a prefix substring
+ * @s: string to check
+ * @accept: string to check against 
+ *
+ * Return: number of bytes os s in accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for(j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (accept[j])
+			return (s + i);
+	}
+	return (0);
+}
+
+
