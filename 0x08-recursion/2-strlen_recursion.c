91b08780ec584fdfc25a5e1a3da0e4d0fc8489ee
peluola <marriegold362@gmail.com>
2022-09-28 16:22:33 -0700

    Task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,7 +5,7 @@
  * _strlen_recursion - calculates the length of a string
  * @s: string to be used
  *
- * Return: Length of the string 
+ * Return: length of the string
  */
 int _strlen_recursion(char *s)
 {
@@ -13,7 +13,7 @@ int _strlen_recursion(char *s)
 
 		if (*s != '\0')
 		{
-			sum ++;
+			sum++;
 			sum += _strlen_recursion(s + 1);
 		}
 	return (sum);
